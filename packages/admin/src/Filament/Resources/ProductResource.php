6fd39dccd80605deeb25e5609882039f47a47842
Beaudinn Greve <beaudinngreve@gmail.com>
2025-01-21 16:27:12 +0100

    fix merge conflict

--- packages/admin/src/Filament/Resources/ProductResource.php
+++ packages/admin/src/Filament/Resources/ProductResource.php
@@ -130,7 +130,7 @@ class ProductResource extends BaseResource
     protected static function getMainFormComponents(): array
     {
         return [
-            static::getBrandFormComponent(),
+            // static::getBrandFormComponent(),
             static::getProductTypeFormComponent(),
             static::getTagsFormComponent(),
         ];
@@ -393,7 +393,7 @@ class ProductResource extends BaseResource
     {
         return parent::getGlobalSearchEloquentQuery()->with([
             'variants',
-            'brand',
+            // 'brand',
             'tags',
         ]);
     }
@@ -403,7 +403,7 @@ class ProductResource extends BaseResource
         return [
             __('lunarpanel::product.table.sku.label') => $record->variants->first()->getIdentifier(),
             __('lunarpanel::product.table.stock.label') => $record->variants->first()->stock,
-            __('lunarpanel::product.table.brand.label') => $record->brand?->name,
+            // __('lunarpanel::product.table.brand.label') => $record->brand?->name,
         ];
     }
 }
--- packages/core/src/Models/Product.php
+++ packages/core/src/Models/Product.php
@@ -50,7 +50,7 @@ class Product extends BaseModel implements Contracts\Product, SpatieHasMedia
     use HasMedia;
     use HasTags;
     use HasTranslations;
-    use HasUrls;
+    //use HasUrls;
     use LogsActivity;
     use Searchable;
     use SoftDeletes;
@@ -74,7 +74,7 @@ class Product extends BaseModel implements Contracts\Product, SpatieHasMedia
         'attribute_data',
         'product_type_id',
         'status',
-        'brand_id',
+        'supplier_id',
     ];
 
     /**
@@ -188,6 +188,11 @@ class Product extends BaseModel implements Contracts\Product, SpatieHasMedia
     public function brand(): BelongsTo
     {
         return $this->belongsTo(Brand::modelClass());
+    }
+
+    public function supplier(): BelongsTo
+    {
+        return $this->belongsTo(Supplier::modelClass());
     }
 
     public function scopeStatus(Builder $query, string $status): Builder
