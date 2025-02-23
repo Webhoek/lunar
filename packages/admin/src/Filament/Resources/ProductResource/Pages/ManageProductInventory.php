39f3a09c5a6a2638ee98d4332c036b5dc1a8872b
Beaudinn Greve <beaudinngreve@gmail.com>
2025-02-23 16:40:08 +0100

    update

--- packages/admin/src/Filament/Resources/ProductResource/Pages/ManageProductInventory.php
+++ packages/admin/src/Filament/Resources/ProductResource/Pages/ManageProductInventory.php
@@ -39,7 +39,7 @@ class ManageProductInventory extends BaseEditRecord
 
     public static function shouldRegisterNavigation(array $parameters = []): bool
     {
-        return $parameters['record']->variants()->count() == 1;
+        return $parameters['record']->variants()->count() == 1 && $parameters['record']->type !== 'dynamic';
     }
 
     public function getBreadcrumb(): string
--- packages/admin/src/Filament/Resources/ProductVariantResource.php
+++ packages/admin/src/Filament/Resources/ProductVariantResource.php
@@ -23,6 +23,10 @@ class ProductVariantResource extends BaseResource
 
     protected static ?string $model = ProductVariant::class;
 
+
+    protected static bool $isScopedToTenant = false;
+
+    
     protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
 
     public static function getLabel(): string
